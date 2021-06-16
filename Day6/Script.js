0e69497e4f9a8a74fa4279269154d84eede46c63
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-06-16 13:21:10 +0530

    using map

--- Day6/Script.js
+++ Day6/Script.js
@@ -1,16 +1,21 @@
+//Q1
+
 function is_array(p1) {
   return Array.isArray(p1);
 }
 
-// console.log(is_array("w3resource"));
-// console.log(is_array([1, 2, 4, 0]));
+console.log(is_array("w3resource"));
+console.log(is_array([1, 2, 4, 0]));
 
+//Q2
 function array_Clone(p1) {
   return p1.slice(0);
 }
 
-// console.log(array_Clone([1, 2, 4, 0]));
-// console.log(array_Clone([1, 2, [4, 0]]));
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+
+//Q3
 
 function first(p1, n = 1) {
   if (n < 0) {
@@ -19,30 +24,38 @@ function first(p1, n = 1) {
   return p1.slice(0, n);
 }
 
-// console.log(first([7, 9, 0, -2]));
-// console.log(first([], 3));
-// console.log(first([7, 9, 0, -2], 3));
-// console.log(first([7, 9, 0, -2], 6));
-// console.log(first([7, 9, 0, -2], -3));
+console.log(first([7, 9, 0, -2]));
+console.log(first([], 3));
+console.log(first([7, 9, 0, -2], 3));
+console.log(first([7, 9, 0, -2], 6));
+console.log(first([7, 9, 0, -2], -3));
+
+//Q4
 
 myColor = ["Red", "Green", "White", "Black"];
-// console.log(myColor.join());
-// console.log(myColor.join());
-// console.log(myColor.join("+"));
+console.log(myColor.join());
+console.log(myColor.join());
+console.log(myColor.join("+"));
 
+//Q5 (1ST METHOD)
 var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
-// let m = new Map();
-// arr1.forEach((item) => {
-//   if (m[item]) {
-//     m[item]++;
-//   } else {
-//     m[item] = 1;
-//   }
-// });
+let m = new Map();
+arr1.forEach((item) => {
+  if (m[item]) {
+    m[item]++;
+  } else {
+    m[item] = 1;
+  }
+});
 
-// for (let [key , value] of m){
-//   if( )
-// }
+for (const [keys, values] of Object.entries(m)) {
+  let x = Object.values(m);
+  if (values == Math.max(...x)) {
+    console.log(keys + " (" + values + " times) ");
+  }
+}
+
+//Q5 (2ND METHOD)
 
 var maximumf = 1;
 var x = 0;
