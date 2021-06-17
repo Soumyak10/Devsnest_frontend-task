dc645f141450cfdcd32eb415e6dddadefa24f948
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-06-18 02:29:55 +0530

    modified (using const div)

--- Day 8/script.js
+++ Day 8/script.js
@@ -1,7 +1,17 @@
+const whiteDivs = [
+  129, 130, 148, 149, 150, 151, 167, 168, 169, 170, 171, 172, 186, 187, 189,
+  190, 192, 193, 206, 207, 208, 209, 210, 211, 212, 213, 228, 231, 247, 249,
+  250, 252, 266, 268, 271, 273,
+];
+
 let container = document.getElementsByClassName("container")[0];
-for (let i = 0; i < 324; i++) {
-  let box = document.createElement("div");
+var box;
+for (let i = 0; i < 400; i++) {
+  box = document.createElement("div");
   box.className = "box";
+  if (whiteDivs.includes(i)) {
+    box.classList.add("white");
+  }
   container.appendChild(box);
 }
 
