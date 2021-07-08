02d45662b1a54edbbeedf9acf6d0d313e9781b3a
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-07-08 15:19:50 +0530

    day 16

---
+++ myapp/src/index.js
@@ -0,0 +1,10 @@
+import REACT from "react";
+import ReactDom from "react-dom";
+import Card from "./Day 16/Card";
+import Card2 from "./Day 16/Card2";
+import Calorimeter from "./Day 17/Calorimeter";
+import "./style.css";
+
+// ReactDom.render(<Card />, document.getElementById("root"));
+// ReactDom.render(<Card2 />, document.getElementById("root"));
+ReactDom.render(<Calorimeter />, document.getElementById("root"));
