1ba0b4183e2dc58f55ab5a21cfafdb8b40aa912a
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-08-05 20:03:10 +0530

    index

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,24 +1,15 @@
 import REACT from "react";
 import ReactDom from "react-dom";
-import Card from "./Day 16/Card";
-import Card2 from "./Day 16/Card2";
-import Calorie_card from "./Day 17/Calorie_card";
-import Board from "./Day 18/Board";
-import App from "./Day 19/App";
-import Calorie_card1 from "./Day 18/Calorie_card1";
-import Calorie_card2 from "./Day 20/Calorie-card2";
-import Calorie_Tracker from "./Day 21/Calorie_Tracker";
-import Meme_gen from "./Day 22/Meme_gen";
-import Customhooks from "./Day 23/Customhooks";
-// import "./style.css";
+import { BrowserRouter } from "react-router-dom";
+import Main_index from "./Main_index";
+import store from "./store";
+import { Provider } from "react-redux";
 
-// ReactDom.render(<Card />, document.getElementById("root"));
-// ReactDom.render(<Card2 />, document.getElementById("root"));
-// ReactDom.render(<Board />, document.getElementById("root"));
-// ReactDom.render(<Calorie_card />, document.getElementById("root"));
-// ReactDom.render(<App />, document.getElementById("root"));
-// ReactDom.render(<Calorie_card1 />, document.getElementById("root"));
-// ReactDom.render(<Calorie_card2 />, document.getElementById("root"));
-// ReactDom.render(<Calorie_Tracker />, document.getElementById("root"));
-// ReactDom.render(<Meme_gen />, document.getElementById("root"));
-ReactDom.render(<Customhooks />, document.getElementById("root"));
+ReactDom.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Main_index />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
