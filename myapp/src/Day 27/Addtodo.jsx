a4beb6b05df1daf6c6a8060b37b8237f3c8919f6
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-08-05 19:55:28 +0530

    day 27

---
+++ myapp/src/Day 27/Addtodo.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToDo } from "../actions/index";
+import Listtodo from "./Listtodo";
+import { FaPlus } from "react-icons/fa";
+import "./style.css";
+
+const Addtodo = () => {
+  const [item, setitem] = useState("");
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container-27">
+      <h2>Todo-List</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Add your todo"
+          value={item}
+          onChange={(e) => {
+            setitem(e.target.value);
+          }}
+        />
+        <FaPlus
+          style={{
+            fontSize: " 35px",
+            border: "1px solid black",
+            background: "#b1cbbb",
+            height: "35px",
+            width: "69px",
+            margin: "10px",
+          }}
+          onClick={() => {
+            dispatch(addToDo({ title: item, done: false }));
+            setitem("");
+          }}
+        />
+      </div>
+      <Listtodo />
+    </div>
+  );
+};
+
+export default Addtodo;
---
+++ myapp/src/Day 27/Listtodo.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteToDo, toggleToDo } from "../actions/index";
+import { FaTrash, FaCheck } from "react-icons/fa";
+
+const ListTodo = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <div style={{ display: "inline-block" }} className="todo-list">
+            {todo.title}
+            <FaTrash
+              style={{
+                fontSize: " 36px",
+                float: "right",
+                background: "red",
+              }}
+              onClick={() => {
+                dispatch(deleteToDo(index));
+              }}
+            />
+            <FaCheck
+              style={{
+                fontSize: " 36px",
+                marginRight: "10px",
+                float: "right",
+                background: "green",
+              }}
+              onClick={() => dispatch(toggleToDo())}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListTodo;
