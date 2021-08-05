1b02ab8563d6214df6a860e439998e910933181d
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-08-05 20:00:30 +0530

    day 19

--- myapp/src/Day 19/App.jsx
+++ myapp/src/Day 19/App.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 class App extends React.Component {
   render() {
     return (
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", marginTop: "18%", marginLeft: "50%" }}>
         {[1, 2, 3, 4].map((ele, index) => (
           <div key={index}>
             <Button />
