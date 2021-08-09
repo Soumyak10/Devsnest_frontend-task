8b65dc327406b6f45b4e896104fbc55a0e881395
Soumya Kesharwani <kesharwanisoumya10@gmail.com>
2021-08-09 19:34:10 +0530

    reducers

--- myapp/src/reducer/index.js
+++ myapp/src/reducer/index.js
@@ -1,10 +1,14 @@
-import showData from "./showData";
 import { combineReducers } from "redux";
+import showData from "./showData";
 import todoReducer from "./todoReducer";
+import placeDataReducer from "./placeDataReducer";
+import placeReducer from "./placeReducer";
 
 const rootReducer = combineReducers({
   showData: showData,
   todos: todoReducer,
+  place: placeReducer,
+  placeData: placeDataReducer,
 });
 
 export default rootReducer;
